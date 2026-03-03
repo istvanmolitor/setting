10ad79ee2a8572f53e74bf05176750a9939d074c
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-03-03 13:38:49 +0100

    api

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Molitor\Setting\Http\Controllers\Api\SettingApiController;
 
-Route::prefix('api/setting')/*->middleware(['api', 'auth:sanctum'])*/->group(function () {
+Route::prefix('api/setting')->middleware(['api', 'auth:sanctum'])->group(function () {
     Route::get('/', [SettingApiController::class, 'index'])->name('setting.api.index');
     Route::get('/{slug}', [SettingApiController::class, 'show'])->name('setting.api.show');
     Route::post('/{slug}', [SettingApiController::class, 'update'])->name('setting.api.update');
