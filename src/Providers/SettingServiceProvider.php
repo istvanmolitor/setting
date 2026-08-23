e2d3c29768d911172ea71b77c35b251d1c7f0e02
Molitor István <istvanmolitor@gmail.com>
2026-08-23 02:39:14 +0200

    blade-ui

--- src/Providers/SettingServiceProvider.php
+++ src/Providers/SettingServiceProvider.php
@@ -12,7 +12,6 @@ class SettingServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'setting');
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
     }
 
